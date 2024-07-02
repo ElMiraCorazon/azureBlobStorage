6781847d4aa34f280b3571c068f3ca6270692db9
ElMiraCorazon <ElMiraCorazon@gmail.com>
2024-07-02 18:44:32 +0300

    update config settings

---
+++ 1-FizzBuzz.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace C_sharp_Challenges
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 != 0))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if ((i % 3 != 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
